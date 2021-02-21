a04377c27def465a9a64464ca500365a57c3fc20
qinyong521 <70684737+qinyong521@users.noreply.github.com>
2021-02-22 00:11:04 +0800

    Update picsew.js

--- picsew.js
+++ picsew.js
@@ -1,8 +1,8 @@
 /*
 #圈x
-https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/picsew.js
+https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/qinyong521/script/main/picsew.js
 #Loon
-http-response https:\/\/buy\.itunes\.apple\.com\/verifyReceipt script-path=https://raw.githubusercontent.com/ZhiYi-N/Private-Script/master/Scripts/picsew.js, requires-body=true, timeout=10, tag=picsew专业版
+http-response https:\/\/buy\.itunes\.apple\.com\/verifyReceipt script-path=https://raw.githubusercontent.com/qinyong521/script/main/picsew.js, requires-body=true, timeout=10, tag=picsew专业版
 #[mitm]
 hostname=buy.itunes.apple.com
 */
